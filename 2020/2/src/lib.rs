843a55c951e296ad51ad158d1c30f1a7b0436643
jw <jw@jw.fi>
2021-02-15 10:36:08 +0200

    weird part two, diff outcomes for diff ifs

--- 2020/2/src/lib.rs
+++ 2020/2/src/lib.rs
@@ -32,14 +32,15 @@ pub fn parse_input(input: &str) -> AnyhowResult<Input> {
         .map(|line| String::from_str(line).unwrap())
         .collect())
 }
+/**
+Each line gives the password policy and then the password. 
+The password policy indicates the lowest and highest number of times
+a given letter must appear for the password to be valid. 
 
+For example, 1-3 a means that the password must contain "a" 
+at least 1 time and at most 3 times.
+**/
 pub fn part1(_input: &Input) -> Output {
-    // let input = _input.into_iter();
-    // let totals:Option<InputNum> = _input.into_iter()
-    //     .cartesian_product(input)
-    //     .find(|p| p.0 + p.1 == 2020)
-    //     .map(|(a,b)| a * b);
-    // totals.unwrap()
     println!("Total of {:?} lines", _input.len());
     let mut valid_lines: Output = 0;
     for line in _input.into_iter() {
@@ -62,6 +63,31 @@ pub fn part1(_input: &Input) -> Output {
 }
 
 
+/**
+Each policy actually describes two positions in the password, where 
+1 means the first character, 
+2 means the second character, and so on. 
+(Be careful; Toboggan Corporate Policies have no concept of "index zero"!) 
+
+Exactly one of these positions must contain the given letter.
+Other occurrences of the letter are irrelevant for the purposes of 
+policy enforcement.
+**/
 pub fn part2(_input: &Input) -> Output {
-0
+    println!("Total of {:?} lines", _input.len());
+    let mut valid_lines: Output = 0;
+    for line in _input.into_iter() {
+        let caps = VALID_LINE.captures(line).unwrap();
+        let first: Output = caps["minimum"].parse().unwrap();
+        let second: Output = caps["maximum"].parse().unwrap();
+        let letter = &caps["letter"].chars().next();
+        let phrase = &caps["phrase"];
+        let len = phrase.chars().count();
+        let first_char = phrase.chars().nth(first + 1);
+        let second_char = phrase.chars().nth(second + 1);
+        if (first_char == *letter) || (len >= second+1 && second_char == *letter) {
+            valid_lines += 1;
+        }
+    }
+    valid_lines
 }
